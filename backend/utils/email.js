f2c9b02be90e55478e3fbc471c2da1a38b683b4a
sn20034 <saketnamdeoofficial@gmail.com>
2026-08-29 15:35:28 +0530

    Fix Brevo SDK named import syntax

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,9 +1,12 @@
-import * as SibApiV3Sdk from "@getbrevo/brevo";
+import {
+  TransactionalEmailsApi,
+  TransactionalEmailsApiApiKeys,
+} from "@getbrevo/brevo";
 import crypto from "crypto";
 
-const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+const apiInstance = new TransactionalEmailsApi();
 apiInstance.setApiKey(
-  SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey,
+  TransactionalEmailsApiApiKeys.apiKey,
   process.env.BREVO_API_KEY,
 );
 
